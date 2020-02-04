fceeb97c679b2ab0d60e7aeeaa9d54c82cd3c8c4
mohammed-fakhry <mohammed-fakhry@hotmail.com>
2020-02-04 09:49:31 +0200

    add comments for team, Some edits

--- js/first.js
+++ js/first.js
@@ -1,5 +1,20 @@
 
-//Fakhry
+//--------------------------------------------------Fakhry
+//scroll
+$(window).scroll(function () {
+    var scrolling = $(this).scrollTop();
+
+    if (scrolling >= 50) {
+        $('nav').removeClass('navbar-light bg-light')
+        $('nav').addClass('navbar-dark bg-dark')
+    } else if (scrolling <= 50) {
+        $('nav').addClass('navbar-light bg-light')
+        $('nav').removeClass('navbar-dark bg-dark')
+    }
+
+})
+
+// search icon effect
 $("#navSearch i").click(function () {
     $("#navSearch input").show(200, function () {
         $("#navSearch i:first").hide();
@@ -22,7 +37,7 @@ function welcomeLoad() {
         document.getElementById("headerTxt").innerHTML += Headertxt.charAt(t);
         t++;
         setTimeout(welcomeLoad, 50);
-        $('#welcomeTxt button').delay(2000).fadeIn().animate({marginTop: '25px', padding: '10px', fontSize: '1.5em'},500)
+        $('#welcomeTxt button').delay(2000).fadeIn().animate({ marginTop: '25px', padding: '10px', fontSize: '1.5em' }, 500)
     }
 }
 window.onload = function () {
@@ -30,20 +45,33 @@ window.onload = function () {
 };
 // image effects
 $("#welcomeImg").mouseenter(function () {
-    $('#welcomeImg img').animate({top: '-20'})
+    $('#welcomeImg img').animate({ top: '-20' })
 })
 $("#welcomeImg").mouseleave(function () {
-    $('#welcomeImg img').animate({top: '0'})
+    $('#welcomeImg img').animate({ top: '0' })
 })
+
 // Image SlideShow
 $("#welcomeImg > .slides:gt(0)").hide();
-setInterval(function() {
-    $('#welcomeImg > .slides:first')
-      .fadeOut(1000)
-      .next()
-      .fadeIn(1000)
-      .end()
-      .appendTo('#welcomeImg');
-  }, 3000);
+setInterval(function () {
+    $('#welcomeImg .slides:first')
+        .fadeOut(1000)
+        .next()
+        .fadeIn(1000)
+        .end()
+        .appendTo('#welcomeImg');
+}, 3000);
 
-//--------------------------------------------------
+//--------------------------------------------------Fakhry
+
+//--------------------------------------------------Mostafa
+//--------------------------------------------------Mostafa
+
+//--------------------------------------------------Yasser
+//--------------------------------------------------Yasser
+
+//--------------------------------------------------Anas
+//--------------------------------------------------Anas
+
+//--------------------------------------------------Shireen
+//--------------------------------------------------Shireen
