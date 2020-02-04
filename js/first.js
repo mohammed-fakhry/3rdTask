0d907ddb102a88ca1e5dccfdc150195c20e97f9b
mohammed-fakhry <mohammed-fakhry@hotmail.com>
2020-02-04 12:11:55 +0200

    Get ready for SecoundSection

--- js/first.js
+++ js/first.js
@@ -37,7 +37,8 @@ function welcomeLoad() {
         document.getElementById("headerTxt").innerHTML += Headertxt.charAt(t);
         t++;
         setTimeout(welcomeLoad, 50);
-        $('#welcomeTxt button').delay(2000).fadeIn().animate({ marginTop: '25px', padding: '10px', fontSize: '1.5em' }, 500)
+        $('#welcomeTxt button').delay(2000).fadeIn(500)
+        $('#headerDescr').delay(2500).fadeIn(600)
     }
 }
 window.onload = function () {
@@ -51,16 +52,32 @@ $("#welcomeImg").mouseleave(function () {
     $('#welcomeImg img').animate({ top: '0' })
 })
 
-// Image SlideShow
-$("#welcomeImg > .slides:gt(0)").hide();
+//SlideShow
+$("#welcomeImg .slides:gt(0)").hide();
 setInterval(function () {
+    let headImgSrc = $("#welcomeImg .slides").attr("src")
+    console.log(headImgSrc)
+    //text SlideShow
+    if (headImgSrc == "Source/header3.jpg") {
+        $('#welcomeTxt h2').html("Mikiya")
+        $('#welcomeTxt p').html("is a Product Designer with a minimalistic portfolio that showcases his work through strong photography and subtle animations. His full site was originally created in Japanese and then translated into English, helping demonstrate the international scalability of his design.")
+    } else if (headImgSrc == "Source/header.jpg") {
+        $('#welcomeTxt h2').html("Revols")
+        $('#welcomeTxt p').html("Revols takes a bold approach on its website by enlarging its small, sophisticated headphones using macro photography and video. The photography and video make the site incredibly product-focused, so its users slowly become more and more immersed in the experience these headphones can give you. The large font treatment on this site is also a nice touch and adds to the larger-than-life earbud photography.")
+    } else if (headImgSrc == "Source/header2.jpg") {
+        $('#welcomeTxt h2').html("uBear")
+        $('#welcomeTxt p').html("uBear designs, develops and produces mobile accessories and related products for urban-minded peoples driven by energy of big cities.")
+    }
+    //image SlideShow
     $('#welcomeImg .slides:first')
         .fadeOut(1000)
         .next()
         .fadeIn(1000)
         .end()
         .appendTo('#welcomeImg');
-}, 3000);
+}, 5000);
+
+
 
 //--------------------------------------------------Fakhry
 
