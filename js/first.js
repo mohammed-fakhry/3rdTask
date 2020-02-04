38861bb0036c76a407f0990f7e02f7db82f60aa4
mohammed-fakhry <mohammed-fakhry@hotmail.com>
2020-02-04 08:07:01 +0200

    welcome images

--- js/first.js
+++ js/first.js
@@ -13,4 +13,34 @@ $("#navSearch i").next().click(function () {
         $("#navSearch i:first").next().hide();
     })
 });
+
+// welcome Texy
+var t = 0;
+var Headertxt = 'Add Your brilliant web site Here.';
+function welcomeLoad() {
+    if (t < Headertxt.length) {
+        document.getElementById("headerTxt").innerHTML += Headertxt.charAt(t);
+        t++;
+        setTimeout(welcomeLoad, 50);
+        $('#welcomeTxt button').delay(1800).fadeIn(900)
+    }
+}
+window.onload = function () {
+    welcomeLoad();
+};
+// image effects
+$("#welcomeImg img").hover(function () {
+    $(this).toggleClass("shadow")
+})
+// Image SlideShow
+$("#welcomeImg > .slides:gt(0)").hide();
+setInterval(function() {
+    $('#welcomeImg > .slides:first')
+      .fadeOut(1000)
+      .next()
+      .fadeIn(1000)
+      .end()
+      .appendTo('#welcomeImg');
+  }, 3000);
+
 //--------------------------------------------------
