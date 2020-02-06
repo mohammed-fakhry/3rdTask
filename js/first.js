1a80dbac33dec9dcc8bd74b9bd66e6b5593c66a9
mohammed-fakhry <mohammed-fakhry@hotmail.com>
2020-02-06 05:49:15 +0200

    slideShow updates

--- js/first.js
+++ js/first.js
@@ -41,54 +41,60 @@ function welcomeLoad() {
 }
 window.onload = function () {
     welcomeLoad();
+    resetInterval();
     $('#welcomeImg img').animate({ top: '20', left: '0' }, 2100); // header Image's Welcome Animation
 };
 // image effects
 $("#welcomeImg").mouseenter(function () {
     $('#welcomeImg img').animate({ top: '-20', left: '-10' })
     $('#headerDescr h2').animate({ fontSize: '2.5em' })
+    clearInterval(ImageSlideShow)
 })
 $("#welcomeImg").mouseleave(function () {
     $('#welcomeImg img').animate({ top: '20', left: '0' })
     $('#headerDescr h2').animate({ fontSize: '2em' })
+    resetInterval()
 })
 
 //SlideShow
-$("#welcomeImg .slides:gt(0)").hide();
-setInterval(function () {
-    let headImgSrc = $("#welcomeImg .slides").attr("src")
-    console.log(headImgSrc)
-    //text SlideShow
-    if (headImgSrc == "Source/header3.jpg") {
-        $("#headerDescr").addClass("bg-light")
-        $("#headerDescr").removeClass("bg-dark text-light")
-        $('#welcomeTxt h2').html("Mikiya")
-        $('#welcomeTxt p').html("is a Product Designer with a minimalistic portfolio that showcases his work through strong photography and subtle animations. His full site was originally created in Japanese and then translated into English, helping demonstrate the international scalability of his design.")
-    } else if (headImgSrc == "Source/header.jpg") {
-        $("#headerDescr").addClass("bg-dark text-light")
-        $("#headerDescr").removeClass("bg-light")
-        $('#welcomeTxt h2').html("Revols")
-        $('#welcomeTxt p').html("Revols takes a bold approach on its website by enlarging its small, sophisticated headphones using macro photography and video. The photography and video make the site incredibly product-focused, so its users slowly become more and more immersed in the experience these headphones can give you.")
-    } else if (headImgSrc == "Source/header2.jpg") {
-        $("#headerDescr").addClass("backColor")
-        $("#headerDescr").removeClass("bg-dark text-light")
-        $('#welcomeTxt h2').html("uBear")
-        $('#welcomeTxt p').html("uBear designs, develops and produces mobile accessories and related products for urban-minded peoples driven by energy of big cities.")
-    }
-    //image SlideShow
-    $('#welcomeImg .slides:first')
-        .fadeOut(1000)
-        .next()
-        .fadeIn(1000)
-        .end()
-        .appendTo('#welcomeImg');
-}, 5000);
+function resetInterval() {
+    $("#welcomeImg .slides:gt(0)").hide();
+    ImageSlideShow = setInterval(function () {
+        let headImgSrc = $("#welcomeImg .slides").attr("src")
+        console.log(headImgSrc)
+        //text SlideShow
+        if (headImgSrc == "Source/header3.jpg") {
+            $("#headerDescr").addClass("bg-light")
+            $("#headerDescr").removeClass("bg-dark text-light")
+            $('#welcomeTxt h2').html("Mikiya")
+            $('#welcomeTxt p').html("is a Product Designer with a minimalistic portfolio that showcases his work through strong photography and subtle animations. His full site was originally created in Japanese and then translated into English, helping demonstrate the international scalability of his design.")
+        } else if (headImgSrc == "Source/header.jpg") {
+            $("#headerDescr").addClass("bg-dark text-light")
+            $("#headerDescr").removeClass("bg-light")
+            $('#welcomeTxt h2').html("Revols")
+            $('#welcomeTxt p').html("Revols takes a bold approach on its website by enlarging its small, sophisticated headphones using macro photography and video. The photography and video make the site incredibly product-focused, so its users slowly become more and more immersed in the experience these headphones can give you.")
+        } else if (headImgSrc == "Source/header2.jpg") {
+            $("#headerDescr").addClass("backColor")
+            $("#headerDescr").removeClass("bg-dark text-light")
+            $('#welcomeTxt h2').html("uBear")
+            $('#welcomeTxt p').html("uBear designs, develops and produces mobile accessories and related products for urban-minded peoples driven by energy of big cities.")
+        }
+        //image SlideShow
+        $('#welcomeImg .slides:first')
+            .fadeOut(1000)
+            .next()
+            .fadeIn(1000)
+            .end()
+            .appendTo('#welcomeImg');
+    }, 4000);
+}
+
 
 //sideBar
 
 $('#sidebarToggle').click(function () {
-    let sideBarWidth = $('#sidebar').height()
-    if (sideBarWidth == 0) { //open sideBar
+    let sideBarHeight = $('#sidebar').height()
+    if (sideBarHeight == 0) { //open sideBar
         $('#sidebar').addClass("card").animate({ height: '50%' }, function () {
             $('#sidebarBtns').show()
         })
@@ -96,17 +102,17 @@ $('#sidebarToggle').click(function () {
         $('#sidebarToggle').removeClass("navbar-toggler-icon").addClass("fa fa-chevron-up")
     } else { //close sideBar
         $('#sidebarBtns').hide()
-        $('#sidebar').removeClass("card").animate({ height: '0px' },200)
+        $('#sidebar').removeClass("card").animate({ height: '0px' }, 200)
         $('#fadeEffect').hide()
-        $('#sidebarToggle').removeClass("fa fa-chevron-up").addClass("navbar-toggler-icon").animate({marginLeft: '5px'})
+        $('#sidebarToggle').removeClass("fa fa-chevron-up").addClass("navbar-toggler-icon")
     }
-    console.log(sideBarWidth)
+    console.log(sideBarHeight)
 })
 $("#fadeEffect").click(function () { //close sideBar
     $('#sidebarBtns').hide()
-    $('#sidebar').removeClass("card").animate({ height: '0px' },200)
+    $('#sidebar').removeClass("card").animate({ height: '0px' }, 200)
     $('#fadeEffect').hide()
-    $('#sidebarToggle').removeClass("fa fa-chevron-up").addClass("navbar-toggler-icon").animate({marginLeft: "5px"})
+    $('#sidebarToggle').removeClass("fa fa-chevron-up").addClass("navbar-toggler-icon")
 })
 
 $('#sidebar button').hover(function () {
